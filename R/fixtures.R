707b861b86c1ed90eb5a54f5f9991a04f6720962
Soumya Ray <soumya.ray@gmail.com>
2023-11-12 11:41:25 +0800

    Refactor: Move gateway and monadic functions used by howto() into their own files

--- R/fixtures.R
+++
@@ -1,5 +0,0 @@
-# require(httptest)
-#
-# start_capturing()
-# air::howto("zip two vectors together pairwise as characters")
-# stop_capturing()
---
+++ R/result.R
@@ -0,0 +1,10 @@
+# Acts as a simple Result monad for R
+result <- function(success = TRUE, status = "ok", message = "done") {
+  list(success = success, status = status, message = message)
+}
+
+# Makes calling function to return with a Result
+return_with <- function(success = TRUE, status = "ok", message = "done"){
+  res <- result(success = success, status = status, message = message)
+  do.call(return, list(res), envir = sys.frame(-1))
+}
