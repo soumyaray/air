d5632f219c73019a1abeda8d218fcc2116ce2862
Soumya Ray <soumya.ray@gmail.com>
2023-11-12 13:14:04 +0800

    Test: Safely vcr with api key

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,5 +1,8 @@
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
+  filter_sensitive_data = list(
+    "<<<api_key>>>" = air:::get_secret("openai-key")$message
+  ),
   dir = vcr::vcr_test_path("fixtures")
 ))
 vcr::check_cassette_names()
