2f0398fb5b9b08d87b192b342df058948f5ed3e6
Soumya Ray <soumya.ray@gmail.com>
2023-11-14 11:02:38 +0800

    tests: protect global env vars during tests of env vars

--- tests/testthat/test-credentials.R
+++ tests/testthat/test-credentials.R
@@ -1,5 +1,5 @@
 test_that("BAD: catches an invalid API key", {
-  bad <- \() {
+  bad_invalid_api_key <- \() {
     vcr::use_cassette("openai-bad-invalid-api-key", {
       air::howto("How do I get the first element of a list?")
     })
@@ -9,21 +9,20 @@ test_that("BAD: catches an invalid API key", {
   key <- Sys.getenv("OPENAI_KEY")
   Sys.setenv("OPENAI_KEY" = "foobar")
   # Should throw a helpful error
-  expect_error(bad(), "Incorrect")
+  expect_error(bad_invalid_api_key(), "Incorrect")
   Sys.setenv("OPENAI_KEY" = key)
 })
 
 test_that("BAD: catches an missing API key", {
-  bad <- \() {
+  bad_no_api_key <- \() {
+    key <- Sys.getenv("OPENAI_KEY")
+    on.exit(Sys.setenv("OPENAI_KEY" = key))
+
     vcr::use_cassette("openai-bad-missing-api-key", {
+      Sys.unsetenv("OPENAI_KEY")
       air::howto("How do I get the first element of a list?")
     })
   }
 
-  # Unset API key env var, and let VCR will use fake replacement key
-  key <- Sys.getenv("OPENAI_KEY")
-  Sys.unsetenv("OPENAI_KEY")
-  # Should throw a helpful error
-  expect_error(bad(), "set your OpenAI key and model")
-  Sys.setenv("OPENAI_KEY" = key)
+  expect_error(bad_no_api_key(), "set your OpenAI key and model")
 })
